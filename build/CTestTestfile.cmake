db416684fd41afb7798341a0a82a18d91ba39e50
LegendCancel <483110994@qq.com>
2022-12-04 20:02:08 +0800

    simulate_v3.0   3d转2d后调用move_base

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,8 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("velodyne_simulator/velodyne_description")
+subdirs("velodyne_simulator/velodyne_simulator")
 subdirs("racebot_control")
 subdirs("urdf_learn_gazebo")
+subdirs("velodyne_simulator/velodyne_gazebo_plugins")
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,8 +131,11 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/lc/urdf_ws/build/gtest/cmake_install.cmake")
+  include("/home/lc/urdf_ws/build/velodyne_simulator/velodyne_description/cmake_install.cmake")
+  include("/home/lc/urdf_ws/build/velodyne_simulator/velodyne_simulator/cmake_install.cmake")
   include("/home/lc/urdf_ws/build/racebot_control/cmake_install.cmake")
   include("/home/lc/urdf_ws/build/urdf_learn_gazebo/cmake_install.cmake")
+  include("/home/lc/urdf_ws/build/velodyne_simulator/velodyne_gazebo_plugins/cmake_install.cmake")
 
 endif()
 
